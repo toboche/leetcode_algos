697dcf49dacf38a4e3686b0c0ffe07a09576cd17
toboche <toboche@gmail.com>
2022-03-09 20:49:34 +0100

    Add Binary - passing tests but leetcode failing because of old std lib

--- src/main/kotlin/AddBinary.kt
+++ src/main/kotlin/AddBinary.kt
@@ -1,5 +1,19 @@
+import java.math.BigInteger
+
 class AddBinary {
     fun addBinary(a: String, b: String): String {
-        return ""
+        return intToBinaryString((binaryToInt(a) + binaryToInt(b)))
+    }
+
+    private fun intToBinaryString(bigInteger: BigInteger): String {
+        return bigInteger.toString(2)
+    }
+
+    private fun binaryToInt(str: String): BigInteger {
+        return str.withIndex()
+            .map { it.value.digitToInt().toBigInteger().shl(str.length - 1 - it.index) }
+            .fold(BigInteger.ZERO) { acc, num ->
+                acc + num
+            }
     }
 }
