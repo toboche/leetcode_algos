0ba1c9429682039deea3e55edcc8615ee57962d3
toboche <toboche@gmail.com>
2022-03-16 20:07:46 +0100

    Validate IP Address - v4

--- src/main/kotlin/ValidateIpAddress.kt
+++ src/main/kotlin/ValidateIpAddress.kt
@@ -4,9 +4,18 @@ class ValidateIpAddress {
             .map { it.toIntOrNull() }
             .map { (0..255).contains(it) }
 
-        if (ipV4.count() == 4) {
+        if (ipV4.count { it } == 4 && ipV4.count() == 4) {
             return "IPv4"
         }
+
+        val ipV6 = queryIP.split(':')
+            .map { it.toIntOrNull(16) }
+            .map { (0.."ffff".toInt(16)).contains(it) }
+
+        if (ipV6.count { it } == 8 && ipV6.count() == 8) {
+            return "IPv6"
+        }
+
         return "Neither"
     }
 }
