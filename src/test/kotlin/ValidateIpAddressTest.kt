4f1bc847f6feefc88aefd5f3b6a1f05bcebf2382
toboche <toboche@gmail.com>
2022-03-16 20:02:03 +0100

    Validate IP Address - v4

--- src/test/kotlin/ValidateIpAddressTest.kt
+++ src/test/kotlin/ValidateIpAddressTest.kt
@@ -10,6 +10,20 @@ class ValidateIpAddressTest {
     }
 
     @Test
+    fun testIpV4NotPassing() {
+        val actual = ValidateIpAddress().validIPAddress("172.16.254.1.1")
+
+        assertEquals("Neither", actual)
+    }
+
+    @Test
+    fun testIpV4NotPassing2() {
+        val actual = ValidateIpAddress().validIPAddress("172.16.254..1")
+
+        assertEquals("Neither", actual)
+    }
+
+    @Test
     fun testIpv6() {
         val actual = ValidateIpAddress().validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334")
 
