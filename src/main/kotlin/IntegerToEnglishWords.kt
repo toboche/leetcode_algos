63f1e7775507b8c8430d0e0e582b08a2fcc943c1
toboche <toboche@gmail.com>
2022-03-19 18:48:09 +0100

    Integer To English Word - up to 00

--- src/main/kotlin/IntegerToEnglishWords.kt
+++ src/main/kotlin/IntegerToEnglishWords.kt
@@ -1,6 +1,6 @@
 class IntegerToEnglishWords {
     val digits = listOf(
-        "Zero",
+        null,
         "One",
         "Two",
         "Three",
@@ -10,6 +10,17 @@ class IntegerToEnglishWords {
         "Seven",
         "Eight",
         "Nine"
+    )
+    val elevenToNineteen = listOf(
+        "Eleven",
+        "Twelve",
+        "Thirteen",
+        "Fourteen",
+        "Fifteen",
+        "Sixteen",
+        "Seventeen",
+        "Eighteen",
+        "Nineteen"
     )
 
     val tens = listOf(
@@ -26,9 +37,28 @@ class IntegerToEnglishWords {
     )
 
     fun numberToWords(num: Int): String {
-        return listOf(
-            tens[num / 10],
+        if (num == 0) {
+            return "Zero"
+        }
+        val tens = if ((20..99).contains(num % 100)) {
+            tens[num / 10]
+        } else {
+            null
+        }
+        val teens = if ((11..19).contains(num % 100)) {
+            elevenToNineteen[num % 100 - 11]
+        } else {
+            null
+        }
+        val digit = if (!(11..19).contains(num % 100)) {
             digits[num % 10]
+        } else {
+            null
+        }
+        return listOf(
+            tens,
+            teens,
+            digit
         )
             .fold("") { acc, it ->
                 if (it.isNullOrBlank()) acc else "$acc $it"
--- src/test/kotlin/IntegerToEnglishWordsTest.kt
+++ src/test/kotlin/IntegerToEnglishWordsTest.kt
@@ -10,9 +10,30 @@ class IntegerToEnglishWordsTest {
     }
 
     @Test
+    fun testSingleZero() {
+        val actual = IntegerToEnglishWords().numberToWords(0)
+
+        assertEquals(actual, "Zero")
+    }
+
+    @Test
     fun test54() {
         val actual = IntegerToEnglishWords().numberToWords(54)
 
         assertEquals(actual, "Fifty Four")
+    }
+
+    @Test
+    fun test11() {
+        val actual = IntegerToEnglishWords().numberToWords(11)
+
+        assertEquals(actual, "Eleven")
+    }
+
+    @Test
+    fun test19() {
+        val actual = IntegerToEnglishWords().numberToWords(19)
+
+        assertEquals(actual, "Nineteen")
     }
 }
