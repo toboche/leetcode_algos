cd3bcb4000faa2800ac13cd74cfdfca82fe5435f
toboche <toboche@gmail.com>
2022-03-26 09:56:46 +0100

    Merge Two Sorted Lists - tests refactor«

--- src/test/kotlin/MergeTwoSortedListsTest.kt
+++ src/test/kotlin/MergeTwoSortedListsTest.kt
@@ -1,18 +1,37 @@
+import ListNode.Companion.fromItems
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import kotlin.test.assertEquals
 
-internal class MergeTwoSortedListsTest {
+@RunWith(Parameterized::class)
+internal class MergeTwoSortedListsTest(
+    val first: List<Int>,
+    val second: List<Int>,
+    val expected: List<Int>,
+) {
+
+    companion object {
+        @Parameterized.Parameters
+        @JvmStatic
+        fun primeNumbers(): Collection<*> {
+            return listOf(
+                arrayOf<Any>(listOf(1, 2, 4), listOf(1, 3, 4), listOf(1, 1, 2, 3, 4, 4)),
+                arrayOf<Any>(listOf<Int>(), listOf<Int>(), listOf<Int>()),
+            )
+        }
+    }
+
+
     @Test
     fun `merge simple lists`() {
         val actual = MergeTwoSortedLists().mergeTwoLists(
-            ListNode.fromItems(listOf(1, 2, 4)),
-            ListNode.fromItems(listOf(1, 3, 4))
+            fromItems(first),
+            fromItems(second)
         )
 
         assertEquals(
-            ListNode.fromItems(
-                listOf(1, 1, 2, 3, 4, 4),
-            ),
+            fromItems(expected),
             actual
         )
     }
