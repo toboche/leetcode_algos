ccf67b70284ee4fe4d07d1da9c803479d7e0a8b3
toboche <toboche@gmail.com>
2022-03-26 09:53:00 +0100

    Merge Two Sorted Lists - passing first case

--- src/test/kotlin/MergeTwoSortedListsTest.kt
+++ src/test/kotlin/MergeTwoSortedListsTest.kt
@@ -1,8 +1,19 @@
 import org.junit.Test
+import kotlin.test.assertEquals
 
 internal class MergeTwoSortedListsTest {
     @Test
     fun `merge simple lists`() {
+        val actual = MergeTwoSortedLists().mergeTwoLists(
+            ListNode.fromItems(listOf(1, 2, 4)),
+            ListNode.fromItems(listOf(1, 3, 4))
+        )
 
+        assertEquals(
+            ListNode.fromItems(
+                listOf(1, 1, 2, 3, 4, 4),
+            ),
+            actual
+        )
     }
 }
