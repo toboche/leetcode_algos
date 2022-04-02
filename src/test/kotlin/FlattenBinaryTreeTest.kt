974196f917f43b03b83cd4c489a9042def9c3c4c
toboche <toboche@gmail.com>
2022-04-02 12:28:09 +0200

    Flatten Binary Tree - simple test 2

--- src/test/kotlin/FlattenBinaryTreeTest.kt
+++ src/test/kotlin/FlattenBinaryTreeTest.kt
@@ -1,9 +1,18 @@
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class FlattenBinaryTreeTest {
     @Test
     fun `test null not crashes`() {
         FlattenBinaryTree().flatten(null)
+    }
+
+    @Test
+    fun `test single item`() {
+        val root = TreeNode(1)
+
+        FlattenBinaryTree().flatten(root)
+
+        assertEquals(1, root.`val`)
     }
 }
