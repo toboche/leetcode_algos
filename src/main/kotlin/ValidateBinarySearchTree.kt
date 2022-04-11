909a3699baff21316ecb7a73a8dbf601b5a163b6
toboche <toboche@gmail.com>
2022-04-11 12:16:02 +0200

    Validate Binary Search Tree - solution 1

--- src/main/kotlin/ValidateBinarySearchTree.kt
+++ src/main/kotlin/ValidateBinarySearchTree.kt
@@ -1,19 +1,25 @@
 var ti = TreeNode(5)
 var v = ti.`val`
 
+//2 -> validate(2,min, max)
+//1 -> validate(1, 2, max)
+//3 -> validate(3, min, 2)
+
 class ValidateBinarySearchTree {
     fun isValidBST(root: TreeNode?): Boolean {
+        if (root == null) {
+            return true
+        }
+        fun validate(node: TreeNode, min: Int, max: Int): Boolean {
+            val thisOk = node.`val` > min && node.`val` < max
+            val leftOk = node.left?.let { left ->
+                validate(left, min, node.`val`)
+            } ?: true
 
-        fun validate(root: TreeNode?, low: Int, high: Int): Boolean {
-            if (root == null) {
-                return true
-            }
-
-            if (root.left!!.`val` >= low || (root.right!!.`val` <= high)) {
-                return false
-            }
-
-            return validate(root.right, root.`val`, high) && validate(root.left, low, root.`val`)
+            val rightOk = node.right?.let { right ->
+                validate(right, node.`val`, max)
+            } ?: true
+            return thisOk && leftOk && rightOk
         }
 
         return validate(root, Int.MIN_VALUE, Int.MAX_VALUE)
--- src/test/kotlin/ValidateBinarySearchTreeTest.kt
+++ src/test/kotlin/ValidateBinarySearchTreeTest.kt
@@ -1,10 +1,19 @@
-import org.junit.Assert.*
+import junit.framework.Assert.assertFalse
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class ValidateBinarySearchTreeTest {
     @Test
     fun test1() {
+        val actual = ValidateBinarySearchTree().isValidBST(TreeNode.from(listOf(2, 1, 3)))
 
-//        ValidateBinarySearchTree().isValidBST()
+        assertTrue(actual)
+    }
+
+    @Test
+    fun test2() {
+        val actual = ValidateBinarySearchTree().isValidBST(TreeNode.from(listOf(5, 1, 4, null, null, 3, 6)))
+
+        assertFalse(actual)
     }
 }
