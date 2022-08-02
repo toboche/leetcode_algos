e52721790ca0eeff180bf7dd86d4426ac935c531
toboche <toboche@gmail.com>
2022-08-02 20:08:07 +0200

    LRU Cache - simple solution with the usage of a LinkedHashMap to get the average O(n) time for all the operations: get, put, and oldest cache item eviction.
    This was the easy mode.

--- src/test/kotlin/LRUCacheWithLinkedHashMapTest.kt
+++ src/test/kotlin/LRUCacheWithLinkedHashMapTest.kt
@@ -9,23 +9,23 @@ class LRUCacheWithLinkedHashMapTest {
 
         lRUCacheWithLinkedHashMap.put(2, 2) // cache is {1=1, 2=2}
 
-        assertThat(lRUCacheWithLinkedHashMap.get(1)).isEqualTo(1) // return 1
+        assertThat(lRUCacheWithLinkedHashMap.get(1))
+            .isEqualTo(1)
 
         lRUCacheWithLinkedHashMap.put(3, 3) // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
 
         assertThat(lRUCacheWithLinkedHashMap.get(2))
-            .isEqualTo(-1)// returns -1 (not found)
+            .isEqualTo(-1)
 
         lRUCacheWithLinkedHashMap.put(4, 4) // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
 
         assertThat(lRUCacheWithLinkedHashMap.get(1))
-            .isEqualTo(-1)// return -1 (not found)
+            .isEqualTo(-1)
 
         assertThat(lRUCacheWithLinkedHashMap.get(3))
-            .isEqualTo(3)// return 3
+            .isEqualTo(3)
 
         assertThat(lRUCacheWithLinkedHashMap.get(4))
-            .isEqualTo(4)// return 4
-
+            .isEqualTo(4)
     }
 }
