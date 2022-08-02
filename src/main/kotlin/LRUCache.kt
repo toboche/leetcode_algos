2f91a9d24b96158af978a584263684be143f14f7
toboche <toboche@gmail.com>
2022-08-02 20:06:46 +0200

    LRU Cache - simple solution with the usage of a LinkedHashMap to get the average O(n) time for all the operations: get, put, and oldest cache item eviction.
    This was the easy mode.

---
+++ src/main/kotlin/LRUCache.kt
@@ -0,0 +1,15 @@
+class LRUCache(capacity: Int) {
+    val linkedHashMap = object : LinkedHashMap<Int, Int>(capacity, 0.75f, true) {
+        override fun removeEldestEntry(eldest: MutableMap.MutableEntry<Int, Int>?): Boolean {
+            return size > capacity
+        }
+    }
+
+    fun get(key: Int): Int {
+        return linkedHashMap.getOrDefault(key, -1)
+    }
+
+    fun put(key: Int, value: Int) {
+        linkedHashMap.put(key, value)
+    }
+}
---
+++ src/test/kotlin/LRUCacheTestWithLinkedHashMap.kt
@@ -0,0 +1,31 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class LRUCacheTestWithLinkedHashMap {
+    @Test
+    fun `sample 1`() {
+        val lRUCache = LRUCache(2)
+        lRUCache.put(1, 1) // cache is {1=1}
+
+        lRUCache.put(2, 2) // cache is {1=1, 2=2}
+
+        assertThat(lRUCache.get(1)).isEqualTo(1) // return 1
+
+        lRUCache.put(3, 3) // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+
+        assertThat(lRUCache.get(2))
+            .isEqualTo(-1)// returns -1 (not found)
+
+        lRUCache.put(4, 4) // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+
+        assertThat(lRUCache.get(1))
+            .isEqualTo(-1)// return -1 (not found)
+
+        assertThat(lRUCache.get(3))
+            .isEqualTo(3)// return 3
+
+        assertThat(lRUCache.get(4))
+            .isEqualTo(4)// return 4
+
+    }
+}
