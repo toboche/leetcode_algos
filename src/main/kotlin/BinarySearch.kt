10d55602e76ecf5cbb3bbe2453c08d8fdb7acb32
toboche <toboche@gmail.com>
2022-09-19 20:31:54 +0200

    Binary Search

--- src/main/kotlin/BinarySearch.kt
+++ src/main/kotlin/BinarySearch.kt
@@ -2,7 +2,6 @@ class BinarySearch {
     fun search(nums: IntArray, target: Int): Int {
         var l = 0
         var r = nums.size - 1
-        var p = (l + r) / 2
         while (l <= r) {
             val p = (l + r) / 2
             if (nums[p] < target) {
