bc82abe18c9d44b0c3445dbab80da78dc459c7b3
toboche <toboche@gmail.com>
2022-09-24 19:38:49 +0200

    Three Number Sum: find all sets of three numbers that sum to the targetSume, returning unique, sorted list - optimal solution: 1. sort input array 2. go through the array only once and have two additional pointers: left, that is initialised to the right of current index; and right that is initialised at the end index of the array. while (left < right){ if(currentSum == target){add to result, increase left, decrease right} else if( currentSum < target){increase left) else{ increase right} }

--- src/main/kotlin/ThreeNumberSum.kt
+++ src/main/kotlin/ThreeNumberSum.kt
@@ -1,18 +1,44 @@
 class ThreeNumberSum {
     fun threeNumberSum(array: MutableList<Int>, targetSum: Int): List<List<Int>> {
         val sorted = array.sorted()
+        val toReturn = mutableListOf<List<Int>>()
+        for (i in sorted.indices) {
+            val current = sorted[i]
+            var left = i + 1
+            var right = sorted.size - 1
+            while (left < right) {
+                val leftValue = sorted[left]
+                val rightValue = sorted[right]
+                val currentSum = leftValue + rightValue + current
+                if (currentSum == targetSum) {
+                    toReturn.add(listOf(current, leftValue, rightValue))
+                    left += 1
+                    right -= 1
+                } else if (currentSum < targetSum) {
+                    left += 1
+                } else {
+                    right -= 1
+                }
+            }
+        }
+
+        return toReturn
+    }
+
+    fun threeNumberSumNotOptimal(array: MutableList<Int>, targetSum: Int): List<List<Int>> {
+        val sorted = array.sorted()
         val toSet = array.toMutableSet()
         val toReturn = mutableListOf<List<Int>>()
-        for(i in sorted.indices){
+        for (i in sorted.indices) {
             val ith = sorted[i]
             toSet.remove(ith)
-            for (j in i+1 until sorted.size){
+            for (j in i + 1 until sorted.size) {
                 val jth = sorted[j]
                 val missing = targetSum - ith - jth
-                if(missing <= jth){
+                if (missing <= jth) {
                     continue
                 }
-                if(toSet.contains(missing)){
+                if (toSet.contains(missing)) {
                     toReturn.add(listOf(ith, jth, missing))
                 }
             }
