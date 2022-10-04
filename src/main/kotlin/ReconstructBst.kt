7c3e8831bb0d7f1a068cb73ad4783685f4a0df28
toboche <toboche@gmail.com>
2022-10-04 13:48:38 +0200

    Reconstruct a BST that is passed as a post-order list of its values. Solution: recursive visiting, after noticing that post order will consist of <head> <left children> <right children>

---
+++ src/main/kotlin/ReconstructBst.kt
@@ -0,0 +1,43 @@
+class ReconstructBst {
+    // This is an input class. Do not edit.
+    open class BST(value: Int, left: BST? = null, right: BST? = null) {
+        var value = value
+        var left = left
+        var right = right
+    }
+
+    fun reconstructBst(preOrderTraversalValues: List<Int>): BST? {
+        fun visit(list: IntArray): BST? {
+            if (list.isEmpty()) return null
+
+            val first = list.first()
+            if (list.size == 1) {
+                return BST(first)
+            }
+
+            val startOfRight = list.withIndex()
+                .firstOrNull { (index, it) ->
+                    it >= first && index != 0
+                }
+                ?.index
+                ?: -1
+            val right = if (startOfRight != -1) {
+                visit(list.copyOfRange(startOfRight, list.count()))
+            } else {
+                null
+            }
+            val left = if (startOfRight != -1 && startOfRight != 1) {
+                visit(list.copyOfRange(1, startOfRight))
+            } else if (startOfRight == -1 && list.count() > 1) {
+                visit(list.copyOfRange(1, list.count()))
+            } else {
+                null
+            }
+
+            return BST(first, left, right)
+        }
+
+
+        return visit(preOrderTraversalValues.toIntArray())
+    }
+}
